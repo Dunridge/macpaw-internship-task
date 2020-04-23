b566ab34114a95b5ff1bf9e733b51eb2c72aefcb
Maksym Polinka <Polinka7max@gmail.com>
2020-04-23 11:34:43 +0300

    added like functionality

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LikeButton from '../assets/svg/LikeButton.svg';
 import MessageButton from '../assets/svg/MessageButton.svg';
 import CelebrityButton from '../assets/svg/CelebrityButton.svg';
+import EmptyLikeButton from '../assets/svg/EmptyLikeButton.svg';
 
 const MainCard = (props) => {
-    console.log(props); 
-    // const [joke, setJoke] = useState(''); //you need to pass the setState method to the Menue component and there set the state (consider using context)
-    // wrong, you don't need to change a card but to render
+    const [cardLike, setCardLike] = useState(false);
+    const [cardLikeCounter, setCardLikeCounter] = useState(1);
+
+    const handleCardLike = () => {
+        // console.log("handleCardLike fired")
+        if(cardLikeCounter % 2 === 1) {
+            setCardLike(true);
+        }
+        if(cardLikeCounter % 2 === 0) {
+            setCardLike(false);
+        }
+        setCardLikeCounter(cardLikeCounter + 1);
+    }
+
     return (
         <div className="main-card-grid main-card">
             <div className="message-button-container">
@@ -18,8 +30,11 @@ const MainCard = (props) => {
             </div>
             <div className="main-last-update">Last update: 1923 hours ago</div>
             <div className="like-button-container">
-                {/* TODO: display your id card  */}
-                <img src={LikeButton} alt="like button" />
+                {cardLike ? (
+                    <img src={LikeButton} alt="like button" onClick={handleCardLike} />
+                ):(
+                    <img src={EmptyLikeButton} alt="empty like button" onClick={handleCardLike}/>
+                )}
             </div>
             <div className="celebrity-button-container">
                 <img src={CelebrityButton} alt="celebrity button" />
